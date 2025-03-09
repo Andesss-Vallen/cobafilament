64edcc565b53a2e3bb214841570139e4f878335c
Nasywadhiya <nasywadhiya246@gmail.com>
2025-03-09 15:20:35 +0700

    diwa

--- database/migrations/2025_03_05_031600_create_treatments_table.php
+++ database/migrations/2025_03_05_031600_create_treatments_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations. rrrr
+     * Run the migrations. rrrrr
      */
     public function up(): void
     {
